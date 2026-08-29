4bd56edefa0e63620fd0ee9ae93e01d3d54be115
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-08-29 20:04:07 -0300

    exibindo as listas de e-mails como uma tabela estilizada

--- resources/views/email-list/index.blade.php
+++ resources/views/email-list/index.blade.php
@@ -6,14 +6,26 @@
     </x-slot>
 
     <x-card>
-        @forelse($emailLists as $list)
-            <p>{{ $list->title }}</p>
-        @empty
+        @unless($emailLists->isEmpty())
+            <x-table :headers="['#', __('Title'), __('Quantity'), __('Actions')]">
+                @foreach($emailLists as $emailList)
+                    <tr>
+                        <x-table.td>{{ $emailList->id }}</x-table.td>
+                        <x-table.td>{{ $emailList->title }}</x-table.td>
+                        <x-table.td>{{ $emailList->subscribers()->count() }}</x-table.td>
+                        <x-table.td>
+                            <x-primary-button type="button">{{ __('Update') }}</x-primary-button>
+                            <x-secondary-button type="button">{{ __('Delete') }}</x-secondary-button>
+                        </x-table.td>
+                    </tr>
+                @endforeach
+            </x-table>
+        @else
             <div class="flex justify-center">
                 <x-link-button :href="route('email-list.create')">
                     {{ __('Create your first email list') }}
                 </x-link-button>
             </div>
-        @endforelse
+        @endunless
     </x-card>
 </x-layouts.app>
